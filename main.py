60f1b0637171981ef0fc9a3285908aa8811acf2c
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-28 21:06:40 +0100

    automated letter test

--- main.py
+++ main.py
@@ -24,10 +24,14 @@ else:
     data.to_dict()
 
 new_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
 if test_date in new_dict:
+    birth_person = new_dict[test_date]
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    
-print(new_dict)
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birth_person["name"])
+        print(contents)
 
 
 
